52745cc0869445a451f2efd149c75fcc0b764f06
Patrick Modafferi <pat.modafferi@gmail.com>
2017-10-09 18:56:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var meals = {}
+var playlist = {Sam Hunt: "House Party"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist
+}
